053b105dbfdda294249708c7cb2527f689f9251c
Shirley Yi <syi@cpg.org>
2023-09-17 13:47:03 -0500

    Document lib_summary

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,14 @@
+#' R library Summary
+#'
+#'Provide a brief summary of the package libraries on your machine.
+#'
+#' @return A two-column `data.frame` containing the count of packages in
+#' each of the user's libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
+
 lib_summary<- function() {
   # tells us what packages are installed
   pkgs<- utils::installed.packages()
